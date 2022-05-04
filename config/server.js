d9972a4193f2812f26a341ab8b274796a3f5f9c7
ASUS\Fraza <adityafraja12@gmail.com>
2022-05-04 16:06:26 +0700

    setup server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,14 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('MY_HEROKU_URL'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
